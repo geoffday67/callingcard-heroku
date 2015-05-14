80f15f2ec943bef0ac2694929c7689f8709e6e26
Geoff Day <geoffday67@gmail.com>
2015-05-14 10:50:46 +0100

    Removed fixed IP

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ var port = process.env.PORT || 5000;
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello calling card world\n');
-}).listen(port, '127.0.0.1');
+}).listen(port);
 console.log('Server running');
